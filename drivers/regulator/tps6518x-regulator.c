267dad049f72678afc6e3215d54fad7f6ed924bb
snehix <surajit.sinha@snehix.com>
2022-02-22 10:40:54 +0000

    22feb 4.10pm

--- drivers/regulator/tps6518x-regulator.c
+++ drivers/regulator/tps6518x-regulator.c
@@ -384,6 +384,7 @@ static int tps6518x_display_enable(struct regulator_dev *reg)
 	unsigned int fld_mask;	  /* register mask for bitfield to modify */
 	unsigned int fld_val;	  /* new bitfield value to write */
 	unsigned int new_reg_val; /* new register value to write */
+	unsigned int read_val;
 	
 	printk("tps6518x_display_enable\n");
 	
@@ -415,8 +416,11 @@ static int tps6518x_display_enable(struct regulator_dev *reg)
 		printk("tps65180_current_Enable_Register=%d,fld_mask=%d,fld_val=%d\n",tps65180_current_Enable_Register,fld_mask,fld_val);
 		new_reg_val = tps65180_current_Enable_Register = to_reg_val(cur_reg_val, fld_mask, fld_val);
 		printk("new_reg_val=%d\n",new_reg_val);
-		new_reg_val=191;
 		tps6518x_reg_write(REG_TPS65180_ENABLE, new_reg_val);
+
+		/* read back the value */
+	   	tps6518x_reg_read(REG_TPS65180_ENABLE, &read_val);
+		printk("read back value = 0x%x\n",read_val);
 		
 	}
 
