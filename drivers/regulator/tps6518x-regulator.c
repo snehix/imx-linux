cc4a51b4afb7a489ab3a73ea43726cfbd8676781
snehix <surajit.sinha@snehix.com>
2022-02-27 20:28:36 +0000

    feb28 feb1.58am

--- drivers/regulator/tps6518x-regulator.c
+++ drivers/regulator/tps6518x-regulator.c
@@ -419,8 +419,6 @@ static int tps6518x_display_enable(struct regulator_dev *reg)
 		printk("new_reg_val=%d\n",new_reg_val);
 		tps6518x_reg_write(REG_TPS65180_ENABLE, new_reg_val);
 		
-		msleep(18);
-
 		/* turn on display regulators */
 		printk("turn on display regulators\n");
 		cur_reg_val = tps65180_current_Enable_Register & 0x3f;
